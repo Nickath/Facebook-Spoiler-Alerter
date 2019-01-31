64d3e589c288d36973b2e5576133ac8037127ff9
Nick Athanasiou <nick_10unipi@hotmail.gr>
2019-01-31 13:23:53 +0200

    extracting links from posts and make asynchronous GET requests to them

--- script.js
+++ script.js
@@ -38,18 +38,50 @@ chrome.storage.sync.get('spoiler_list', function(data){
 	  }
 	  return element;
   }
+
+
+  function findEmbeddedLinks(){
+	  //var matches = elementsFB.querySelectorAll("A"); //to query by element tag
+	 var parentOfLinksCollection = document.getElementsByClassName("_3ekx _29_4");
+	 var arrayOfCollectionLinks = Array.from(parentOfLinksCollection);
+
+	 var links = arrayOfCollectionLinks[0].querySelectorAll("A");
+	 for (var index = 0; index < links.length; index++) {
+		//alert(links[index]);
+		console.log(links[index]);
+		var linkResponse = httpGet(links[index]);
+		alert('Link response is ' + linkResponse);
+	}
+  }
+
+  function httpGet(theUrl)
+  {
+	  var xmlHttp = new XMLHttpRequest();
+	  xmlHttp.open( "GET", theUrl, true ); // false for synchronous request
+	  xmlHttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+	  xmlHttp.send( null );
+	  xmlHttp.onload = function() {
+		console.log(xmlHttp.responseText);
+	  };
+	  return xmlHttp.responseText;
+  }
+
+
   
   function blockSpoil() {
+
+	 
 	  /* the div class of every facebook post*/
      const elementsFB = ['.userContent']
      /* get all divs containing the class .userContent */
-     const elements = document.querySelectorAll(elementsFB)
+	 const elements = document.querySelectorAll(elementsFB)
 
 	 /* iterate through the elements that contain text from the spoilersRegex*/
      elements.forEach(function(element){
 		 if(!element.classList.contains('spoiled') && element.innerText.match(spoilersRegex) !== null) {
 			 console.log(element)
-			 
+			 //var matches = myBox.querySelectorAll("p"); to query by element tag
+			
 			 /* to close all the div call the closest function*/
 			 let parentElement =  closest(element,'userContentWrapper')
 			 const originalPost = parentElement;
@@ -83,6 +115,9 @@ chrome.storage.sync.get('spoiler_list', function(data){
    /*Execute a JavaScript immediately after a page has been loaded: */
    window.onload = function() {
 	   spoilersRegex = new RegExp(spoilers.join('|'),'i') /* creation of incase sensitive regex of the spoilers array */
+	   //find spoiled links
+	   findEmbeddedLinks();
+
 	   blockSpoil()
 	   /* in order to call the function when scrolling event is happening again*/
 	   window.addEventListener('scroll',blockSpoil)
